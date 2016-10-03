8b7dfccd8a5832da3cff16dfc3a40d4cdd9122da
marioosh <marioosh.net@gmail.com>
2016-10-03 10:03:09 +0200

    init repo

---
+++ src/main/java/net/marioosh/spring/SpringWebApp.java
@@ -0,0 +1,81 @@
+package net.marioosh.spring;
+
+import java.util.HashMap;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+
+@Configuration
+@EnableWebMvc
+@Controller
+public class SpringWebApp implements WebApplicationInitializer {
+	
+	@RequestMapping("/")
+	public void pdf(HttpServletResponse response) throws Exception {
+		
+		JasperReport jasperReport = JasperCompileManager.compileReport(SpringWebApp.class.getClassLoader().getResourceAsStream("test.jrxml"));
+		HashMap<String, Object> parameters = new HashMap<String, Object>();
+		JRDataSource dataSource = new JREmptyDataSource();		
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+		
+		JRPdfExporter exporter = new JRPdfExporter();
+		exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+		exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(response.getOutputStream()));
+		
+		response.setContentType("application/pdf");
+		//response.setHeader("Content-Disposition", "attachment; filename=\"test.pdf\"");		
+		exporter.exportReport();
+	}
+	
+	/**
+	 * jsp View Resolver
+	 * @return
+	 */
+	@Bean
+	public ViewResolver internalViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setSuffix(".jsp");
+		/**
+		 * put Your .jsp in src/main/webapp/WEB-INF/views/ directory
+		 */
+		resolver.setPrefix("/WEB-INF/views/");
+		return resolver;
+	}
+
+	/**
+	 * initialize Spring Dispatcher Servlet 
+	 * required Servlet Container 3.0+
+	 */
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(SpringWebApp.class);
+		ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		registration.setLoadOnStartup(1);
+		registration.addMapping("/");			
+	}
+}
